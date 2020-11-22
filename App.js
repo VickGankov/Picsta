0b7bd3622ca1e4899ce277d1de1d9a77d4d5baf2
vickgankov <vasil_gankov@yahoo.com>
2020-11-22 09:10:15 -0800

    Navigation bar and picture taking

--- App.js
+++ App.js
@@ -3,25 +3,26 @@ import React, { Component } from 'react';
 
 import { View, Text } from 'react-native'
 
-import * as firebase from 'firebase'
+import * as firebase from 'firebase';
+import 'firebase/firestore';
 
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './redux/reducers'
 import thunk from 'redux-thunk'
-
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCnL6lmNfdjg7GRmreCMC7Gon8cnKD6hiY",
-  authDomain: "picsta-5c28c.firebaseapp.com",
-  databaseURL: "https://picsta-5c28c.firebaseio.com",
-  projectId: "picsta-5c28c",
-  storageBucket: "picsta-5c28c.appspot.com",
-  messagingSenderId: "982826221843",
-  appId: "1:982826221843:web:768e3edd94030c2e60dae6",
-  measurementId: "G-VC13TPTX83"
+  apiKey: "AIzaSyAGV2HfQSQtFCjD9xbu4RDUiTewa7is8O8",
+  authDomain: "instagram-dev-8b9c4.firebaseapp.com",
+  databaseURL: "https://instagram-dev-8b9c4.firebaseio.com",
+  projectId: "instagram-dev-8b9c4",
+  storageBucket: "instagram-dev-8b9c4.appspot.com",
+  messagingSenderId: "386631361638",
+  appId: "1:386631361638:web:d25aa4b5ec7911c57679a7",
+  measurementId: "G-GQYEJ3XWBV"
 };
 
 if (firebase.apps.length === 0) {
@@ -35,6 +36,8 @@ import LandingScreen from './components/auth/Landing'
 import RegisterScreen from './components/auth/Register'
 import LoginScreen from './components/auth/Login'
 import MainScreen from './components/Main'
+import AddScreen from './components/main/Add'
+import SaveScreen from './components/main/Save'
 
 
 const Stack = createStackNavigator();
@@ -42,7 +45,7 @@ const Stack = createStackNavigator();
 
 export class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       loaded: false,
     }
@@ -87,7 +90,14 @@ export class App extends Component {
 
     return (
       <Provider store={store}>
-        <MainScreen />
+        <NavigationContainer >
+          <Stack.Navigator initialRouteName="Main">
+            <Stack.Screen name="Main" component={MainScreen}  options={{ headerShown: false }} />
+            <Stack.Screen name="Add" component={AddScreen} navigation= {this.props.navigation} />
+            <Stack.Screen name="Save" component={SaveScreen} />
+
+          </Stack.Navigator>
+        </NavigationContainer>
       </Provider>
     )
   }
