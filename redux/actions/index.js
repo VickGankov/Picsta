e502f7c6b6eeff36306c782551a8ad07cea55596
vickgankov <vasil_gankov@yahoo.com>
2020-11-25 04:42:38 -0800

    displaying user info from firestore

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,5 +1,6 @@
-import {USER_STATE_CHANGE} from '../constants/index'
+import {USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE} from '../constants/index'
 import firebase from 'firebase'
+import { SnapshotViewIOS } from 'react-native'
 
 export function fetchUser() {
     return ((dispatch) => {
@@ -18,3 +19,20 @@ export function fetchUser() {
             })
     })
 }
+export function fetchUserPosts() {
+    return ((dispatch) => {
+        firebase.firestore()
+            .collection("posts")
+            .doc(firebase.auth().currentUser.uid)
+            .collection("userPosts")
+            .orderBy("creation", "asc")
+            .get()
+            .then((snapshot) => {
+let posts = snapshot.docs.map(doc => {
+    const data = doc.data();
+    const id = doc.id;
+    return {id, ...data}
+})  
+dispatch({type : USER_POSTS_STATE_CHANGE, posts})
+
+})})}
