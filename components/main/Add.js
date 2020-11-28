c86b2142f4233c732c655301e9aa79848323fd6e
vickgankov <vasil_gankov@yahoo.com>
2020-11-28 03:31:23 -0800

    implementing user search

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
-import { Camera } from 'expo-camera';
-import * as ImagePicker from 'expo-image-picker';
-
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
+import { Camera } from "expo-camera";
+import * as ImagePicker from "expo-image-picker";
 
 export default function Add({ navigation }) {
   const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
@@ -14,12 +13,10 @@ export default function Add({ navigation }) {
   useEffect(() => {
     (async () => {
       const cameraStatus = await Camera.requestPermissionsAsync();
-      setHasCameraPermission(cameraStatus.status === 'granted');
+      setHasCameraPermission(cameraStatus.status === "granted");
 
       const galleryStatus = await ImagePicker.requestCameraRollPermissionsAsync();
-      setHasGalleryPermission(galleryStatus.status === 'granted');
-
-
+      setHasGalleryPermission(galleryStatus.status === "granted");
     })();
   }, []);
 
@@ -28,7 +25,7 @@ export default function Add({ navigation }) {
       const data = await camera.takePictureAsync(null);
       setImage(data.uri);
     }
-  }
+  };
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -44,7 +41,6 @@ export default function Add({ navigation }) {
     }
   };
 
-
   if (hasCameraPermission === null || hasGalleryPermission === false) {
     return <View />;
   }
@@ -55,10 +51,11 @@ export default function Add({ navigation }) {
     <View style={{ flex: 1 }}>
       <View style={styles.cameraContainer}>
         <Camera
-          ref={ref => setCamera(ref)}
+          ref={(ref) => setCamera(ref)}
           style={styles.fixedRatio}
           type={type}
-          ratio={'1:1'} />
+          ratio={"1:1"}
+        />
       </View>
 
       <Button
@@ -69,11 +66,14 @@ export default function Add({ navigation }) {
               ? Camera.Constants.Type.front
               : Camera.Constants.Type.back
           );
-        }}>
-      </Button>
+        }}
+      ></Button>
       <Button title="Take Picture" onPress={() => takePicture()} />
       <Button title="Pick Image From Gallery" onPress={() => pickImage()} />
-      <Button title="Save" onPress={() => navigation.navigate('Save', { image })} />
+      <Button
+        title="Save"
+        onPress={() => navigation.navigate("Save", { image })}
+      />
       {image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
     </View>
   );
@@ -82,11 +82,10 @@ export default function Add({ navigation }) {
 const styles = StyleSheet.create({
   cameraContainer: {
     flex: 1,
-    flexDirection: 'row'
+    flexDirection: "row",
   },
   fixedRatio: {
     flex: 1,
-    aspectRatio: 1
-  }
-
-})
+    aspectRatio: 1,
+  },
+});
